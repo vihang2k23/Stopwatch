cd491fba094227799400e8de34cea0c26daa0fa0
vihang2k23 <vihangp@zignuts.com>
2023-02-08 11:48:30 +0530

    first commit

---
+++ js/clock.js
@@ -0,0 +1,43 @@
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if (hh > 12) {
+    session = "PM";
+  }
+
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
+
+  let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time;
+  var t = setTimeout(function () {
+    currentTime();
+  }, 1000);
+  // console.log(pauseTimer);
+  // pauseTimer.addEventListner("click", () => {
+  //   clearTimeout();
+  // });
+  const pauseTimer = document.getElementById("pauseTimer");
+  pauseTimer.addEventListener("click", () => {
+    clearTimeout(t);
+  });
+
+  const startTimer = document.getElementById("startTimer");
+  startTimer.addEventListener("click", () => {
+    setTimeout(function () {
+      currentTime();
+    }, 1000);
+  });
+  const stopwatch = document.getElementById("stopwatch");
+  stopwatch.addEventListener("click", () => {
+    window.location.replace("./stopwatch.html");
+  });
+}
+
+currentTime();
